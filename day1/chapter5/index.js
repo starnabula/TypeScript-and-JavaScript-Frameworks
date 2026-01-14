6c1bbe74d66ffef27765070caebdeec6f109896c
THEETHAWACH  BANNAKAN <fordeawq234@gmail.com>
2026-01-15 03:05:23 +0700

    Add files via upload

--- day1/chapter5/index.js
+++ day1/chapter5/index.js
@@ -21,6 +21,9 @@ async function fetchData2() {
         const response = await fetch('https://jsonplaceholder.typicode.com/posts/2');
         const data = await response.json();
         console.log(data);
+        const response2 = await fetch('https://jsonplaceholder.typicode.com/users/' + data.userId);
+        const data2 = await response2.json();
+        console.log(data2);
     } catch (error) {
         console.error(error);
     }
